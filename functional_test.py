7023da2d857fb32ef9a11d0c35f83d93b2d35d03
Siavash Aghili <siavashaghili@gmail.com>
2023-04-14 02:49:47 +0100

    add save a post req

--- functional_test.py
+++ functional_test.py
@@ -23,22 +23,25 @@ class newVisitorTest(unittest.TestCase):
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-        
-        inputbox.send_keys('Buy peacock feathers')
+        # inputbox.send_keys('Buy peacock feathers') 
+        inputbox.send_keys('Use peacock feathers to make a fly')
         
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         
         table = self.browser.find_element(By.ID,'id_list_table')
         rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn(
+            '1: Buy peacock feathers' , 
+                     [row.text for row in rows])
+        
+        self.assertIn(
+            '2: Use peacock feathrs to make a fly',
+                      [row.text for row in rows]
+                      )
         
         self.fail('Fininsh the test!')
          
-    
             
         
 if __name__ == '__main__':
--- lists/tests.py
+++ lists/tests.py
@@ -1,18 +1,13 @@
-from django.template.loader import render_to_string
-from django.urls import resolve
 from django.test import TestCase
 
-from lists.views import home_page
-
-# Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_home_page_return_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-        
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': "A new list item"})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
         
